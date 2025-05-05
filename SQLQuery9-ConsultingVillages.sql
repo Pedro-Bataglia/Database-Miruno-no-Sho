0a1f75ad197b465f7b0dca46e9451259f750059b
Pedro-Bataglia <phenriquebataglia@gmail.com>
2025-05-05 14:33:17 -0300

    melhorando consulta

--- SQLQuery9-ConsultingVillages.sql
+++ SQLQuery9-ConsultingVillages.sql
@@ -6,6 +6,7 @@ SELECT * FROM Villages
 
 
 SELECT Clan_name, Origim_Place, age, Village_location
+, count(age) OVER (PARTITION BY Origim_place) as SameOrigim
 FROM Clan
 JOIN Villages
 ON Clan.Origim_Place = Villages.VillageID
